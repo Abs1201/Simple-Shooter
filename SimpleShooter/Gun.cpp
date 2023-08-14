e1b8e10929c5eba4f062968c155c71782f8e487b
KimSungHun <kimtiger1201@naver.com>
2023-08-14 17:26:36 +0900

    SetFocus, MoveToActor

--- SimpleShooter/Gun.cpp
+++ SimpleShooter/Gun.cpp
@@ -20,37 +20,6 @@ AGun::AGun()
 	Mesh->SetupAttachment(Root);
 }
 
-// void AGun::PullTrigger()
-// {
-// 	UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
-// 	APawn* OwnerPawn = Cast<APawn>(GetOwner());
-// 	if(OwnerPawn == nullptr) return;
-// 	AController* OwnerController = OwnerPawn->GetController();
-// 	if(OwnerController == nullptr) return;
-
-// 	FVector Location;
-// 	FRotator Rotation;
-// 	OwnerController->GetPlayerViewPoint(Location, Rotation);
-
-// 	FVector End = Location + Rotation.Vector() * MaxRange;
-	
-// 	//DrawDebugCamera(GetWorld(), Location, Rotation, 90, 2, FColor::Red, true);
-// 	FHitResult Hit;
-// 	bool bSuccess = GetWorld()->LineTraceSingleByChannel(Hit, Location, End, ECollisionChannel::ECC_GameTraceChannel1);
-// 	if (bSuccess)
-// 	{
-// 		FVector ShotDirection = -Rotation.Vector();
-// 		//DrawDebugPoint(GetWorld(), Hit.Location, 20, FColor::Red, true);
-// 		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), HitFlash, Hit.Location, ShotDirection.Rotation());
-
-// 		AActor* HitActor = Hit.GetActor();
-// 		if(HitActor != nullptr){
-// 			FPointDamageEvent DamageEvent(Damage, Hit, ShotDirection, nullptr);
-// 			HitActor->TakeDamage(Damage, DamageEvent, OwnerController, this);
-// 		}
-// 	}
-
-// }
 
 
 void AGun::PullTrigger() 
