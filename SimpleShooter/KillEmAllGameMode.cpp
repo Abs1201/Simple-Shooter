51d19afaeeaade01ae8b148d36765926732a4f60
KimSungHun <kimtiger1201@naver.com>
2023-08-29 03:47:49 +0900

    197

--- SimpleShooter/KillEmAllGameMode.cpp
+++ SimpleShooter/KillEmAllGameMode.cpp
@@ -2,7 +2,8 @@
 
 
 #include "KillEmAllGameMode.h"
-
+#include "EngineUtils.h"
+#include "GameFramework/Controller.h"
 
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
@@ -11,6 +12,22 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
     UE_LOG(LogTemp, Display, TEXT("A Pawn died"));
     APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
     if(PlayerController != nullptr){
-        PlayerController->GameHasEnded(nullptr, false);
+        //PlayerController->GameHasEnded(nullptr, false);
+        EndGame(false);
+    }
+}
+
+void AKillEmAllGameMode::EndGame(bool bIsPlayerWinner)
+{
+    for(AController* Controller: TActorRange<AController>(GetWorld())){
+        // bool bIsPlayerController = Controller->IsPlayerController();
+        // if(bIsPlayerWinner){
+        //     Controller->GameHasEnded(nullptr, bIsPlayerController);
+        // }
+        // else{
+        //     Controller->GameHasEnded(nullptr, !bIsPlayerController);
+        // }
+        bool bIsWinner = Controller->IsPlayerController() == bIsPlayerWinner;
+        Controller->GameHasEnded(Controller->GetPawn(), bIsWinner);
     }
 }
